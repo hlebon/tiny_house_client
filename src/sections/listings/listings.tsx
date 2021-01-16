d21c0dd7544044ab6e36656502c9d4e785d9a610
hans <hgarcia@assanet.com>
2021-01-15 22:35:30 -0500

    feat: delete a value, query mutation

--- src/sections/listings/listings.tsx
+++ src/sections/listings/listings.tsx
@@ -1,5 +1,9 @@
 import { server } from "../../lib";
-import { ListingData } from "./types";
+import {
+  ListingData,
+  DeleteListingData,
+  DeleteListingVariables,
+} from "./types";
 
 const LISTINGS = `
     query Listings {
@@ -17,6 +21,14 @@ const LISTINGS = `
     }
 `;
 
+const DELETE_LISTING = `
+    mutation DeleteListing($id: ID!) {
+        deleteListing(id: $id) {
+            id
+        }
+    }
+`;
+
 interface ListingsProps {
   title: string;
 }
@@ -26,10 +38,25 @@ export function Listings({ title }: ListingsProps) {
     const { data } = await server.fetch<ListingData>({ query: LISTINGS });
     console.log({ data });
   };
+
+  const deleteListing = async () => {
+    const { data } = await server.fetch<
+      DeleteListingData,
+      DeleteListingVariables
+    >({
+      query: DELETE_LISTING,
+      variables: {
+        id: "5fff1e6c1502973e00c18b61",
+      },
+    });
+    console.log({ data });
+  };
+
   return (
     <div>
       <h1>{title}</h1>
       <button onClick={fetchListings}>Query Listing</button>
+      <button onClick={deleteListing}>Delete Listing</button>
     </div>
   );
 }
